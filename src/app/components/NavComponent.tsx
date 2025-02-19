29daa9021d6be95423e19bb98fe7d0a4a9a54ed2
Pat Wilson <pat@wilsons.org.nz>
2025-02-19 11:27:22 +1100

    Added dropdown component to nav. Need to start fetching slugs, titles for shows

--- src/app/components/NavComponent.tsx
+++ src/app/components/NavComponent.tsx
@@ -16,30 +16,18 @@ import {
 
 // import 'gsp-logo'
 import Image from 'next/image'
-
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  )
-}
+import DropdownComponent from './Dropdown'
 
 export default function App() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false)
 
   const menuItems = [
     'Shows',
-    'Projects',
-    'Members',
-    'Community',
-    'About',
-    'Links',
+    // 'Projects',
+    // 'Members',
+    // 'Community',
+    // 'About',
+    // 'Links',
   ]
 
   return (
@@ -82,6 +70,7 @@ export default function App() {
           </NavbarMenuItem>
         ))}
       </NavbarMenu>
+      <DropdownComponent />
     </Navbar>
   )
 }
