a45c7baf8a81a7ef84b726b8be3260228ac53a4d
Pat Wilson <pat@wilsons.org.nz>
2025-02-24 10:59:39 +1100

    WIP 2

--- src/app/components/FloatingGallery.tsx
+++ src/app/components/FloatingGallery.tsx
@@ -11,16 +11,29 @@ const FloatingGallery = () => {
   ]
 
   return (
-    <div className="relative h-[calc(100vh-4rem)] w-full">
+    <div className="relative h-[calc(100vh-4rem)] w-full overflow-x-hidden flex flex-row flex-wrap lg:py-16 p-4 justify-center">
+      <div className="absolute w-full text-center text-xl text-pink-500">
+        testing testing testing testing testing
+      </div>
       {images.map((src, index) => (
         <img
           key={index}
           src={src}
           alt={`Image ${index + 1}`}
-          className="absolute h-1/4 object-contain"
+          className="relative w-1/6 lg:w-1/4 object-contain z-10"
           style={{
-            top: `${Math.random() * 50}%`,
-            left: `${Math.random() * 50 + 25}%`,
+            animation: ` ${10 + index * 2}s infinite ease-in-out`,
+            animationDelay: `-${index * 2}s`,
+          }}
+        />
+      ))}
+      {images.map((src, index) => (
+        <img
+          key={index}
+          src={src}
+          alt={`Image ${index + 1}`}
+          className="relative w-1/6 lg:w-1/4 object-contain z-10"
+          style={{
             animation: ` ${10 + index * 2}s infinite ease-in-out`,
             animationDelay: `-${index * 2}s`,
           }}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import FloatingGallery from './components/FloatingGallery'
 export default function HomePage() {
   return (
     <div>
-      <main className="bg-gsp-green/90 flex flex-col items-center justify-center min-h-[calc(100vh-4rem)]">
+      <main className="bg-gsp-black flex flex-col items-center justify-center min-h-[calc(100vh-4rem)]">
         <FloatingGallery />
       </main>
     </div>
