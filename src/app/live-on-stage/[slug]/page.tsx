4c38fa6bdbef992df3c19744c7111d69d20eb534
Pat Wilson <pat@wilsons.org.nz>
2025-08-11 11:44:38 +1000

    build fixed, params work on slug show page

--- src/app/live-on-stage/[slug]/page.tsx
+++ src/app/live-on-stage/[slug]/page.tsx
@@ -10,21 +10,16 @@ const SHOW_QUERY = `*[_type == "liveOnStage" && slug.current == $slug][0] {
   videos
 }`
 
-interface LiveOnStagePageProps {
-  params: { slug: string }
-}
-
 export default async function LiveOnStagePage({
   params,
-}: LiveOnStagePageProps) {
-  const show = await client.fetch(
-    SHOW_QUERY,
-    { slug: params.slug },
-    { next: { revalidate: 30 } }
-  )
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const show = await client.fetch(SHOW_QUERY, await params, {
+    next: { revalidate: 30 },
+  })
 
   if (!show) {
-    // Optional: Handle case where no show is found
     return (
       <div className="w-screen min-h-screen absolute top-0 left-0 flex flex-col items-center justify-center">
         <p>Show not found :(</p>
